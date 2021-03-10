2e2f4bb84b053b59422150ae1b53ab92d642bb72
Mahier Sydow <mahier.sydow8@gmail.com>
2021-03-10 17:18:59 +0900

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Toasty from "./Toast";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Toasty />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
